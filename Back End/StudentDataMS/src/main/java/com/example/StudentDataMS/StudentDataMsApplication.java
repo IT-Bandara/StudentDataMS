fa9db598be9857dbd292156ad9a6107ba523326a
Isuru <isurutharakabandara@gmail.com>
2024-04-10 14:43:10 +0530

    initialized spring boot project

---
+++ Back End/StudentDataMS/src/main/java/com/example/StudentDataMS/StudentDataMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentDataMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDataMsApplication.class, args);
+	}
+
+}
---
+++ Back End/StudentDataMS/src/test/java/com/example/StudentDataMS/StudentDataMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentDataMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
